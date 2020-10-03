6c31b46cf84326977aa722d69facf71c38ade6ec
estabravo <jbravo.post@gmail.com>
2020-10-02 23:37:20 -0700

    feature: 'completed' class now toggles for li's

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,4 @@
 // check off specific todos by clicking
 $('li').click(e => {
-    $(e.target).css({
-        color: 'gray',
-        textDecoration: 'line-through'
-    });
+    $(e.target).toggleClass('completed');
 });
