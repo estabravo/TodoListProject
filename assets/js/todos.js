561b480904c666b6c2d368b33eca24870e63551d
estabravo <jbravo.post@gmail.com>
2020-10-05 22:27:44 -0700

    feature: input field now generates new li with value

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -10,3 +10,14 @@ $('span').click(e => {
     });
     e.stopPropagation();
 });
+
+$('input[type="text"]').keypress(e =>{
+    // .which method is specifying character code for enter key (13)
+    if(e.which === 13){
+        // grabbing new todo text from input
+        let todoText = $(e.target).val();
+        $(e.target).val('');
+        // create a new li and add to ul
+        $('ul').append(`<li>${todoText} <span>X</span></li>`);
+    };
+});
